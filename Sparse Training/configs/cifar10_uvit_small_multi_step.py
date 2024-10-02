c88667912484856cd043fb26a8d4bcf836566ace
aaa-bbb-111 <214839227@qq.com>
2024-10-02 20:40:47 +0800

    Add files via upload

---
+++ Sparse Training/configs/cifar10_uvit_small_multi_step.py
@@ -0,0 +1,222 @@
+import ml_collections
+
+
+def d(**kwargs):
+    """Helper of creating a config dict."""
+    return ml_collections.ConfigDict(initial_dictionary=kwargs)
+
+
+def get_config():
+    config = ml_collections.ConfigDict()
+
+    config.seed = 1234
+    config.pred = 'noise_pred'
+
+    config.train = d(
+        n_steps=500000,
+        batch_size=16,   #   modify for mem ,  128
+        mode='uncond',
+        log_interval=10,
+        # eval_interval=5000,   #   modify
+        eval_interval=1,
+        # save_interval=50000,
+        save_interval=1,  #   modify
+    )
+
+    config.optimizer = d(
+        name='adamw',
+        lr=0.0002,
+        weight_decay=0.03,
+        betas=(0.99, 0.999),
+    )
+
+    config.lr_scheduler = d(
+        name='customized',
+        warmup_steps=2500
+    )
+
+    config.nnet = d(
+        name='uvit_linear',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet2 = d(
+        name='uvit_step2',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet3 = d(
+        name='uvit_step3',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet4= d(
+        name='uvit_step4',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet7= d(
+        name='uvit_step7',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet8= d(
+        name='uvit_step8',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet15= d(
+        name='uvit_step15',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet16= d(
+        name='uvit_step16',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet31= d(
+        name='uvit_step31',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet32= d(
+        name='uvit_step32',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet6_8= d(
+        name='uvit_step6_8',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet5_8= d(
+        name='uvit_step5_8',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+    config.nnet4_8= d(
+        name='uvit_step4_8',
+        img_size=32,
+        patch_size=2,
+        embed_dim=512,
+        depth=12,
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=False,
+        mlp_time_embed=False,
+        num_classes=-1,
+    )
+
+
+    config.dataset = d(
+        name='cifar10',
+        path='assets/datasets/cifar10',
+        random_flip=True,
+    )
+
+    config.sample = d(
+        sample_steps=1000,
+        n_samples=50000,
+        mini_batch_size=500,
+        algorithm='euler_maruyama_sde',
+        path=''
+    )
+
+    return config
